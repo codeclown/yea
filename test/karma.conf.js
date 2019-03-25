1b1993e68c28cdd90cc4596ab9e20eb1800aa49b
Martti Laine <martti@codeclown.net>
2019-03-25 23:18:56 +0100

    Sauce

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -3,7 +3,7 @@ const helperServer = require('./helper-server');
 module.exports = function(config) {
   // Karma config is some of the most horrifying things I've seen... anyway, here goes
 
-  config.set({
+  const configuration = {
     basePath: '../',
     files: [
       'test/specs/*.js',
@@ -17,9 +17,11 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['ChromeHeadless'],
     singleRun: false,
     concurrency: Infinity,
+
+    // Locally run on Chrome headless
+    browsers: ['ChromeHeadless'],
 
     // Run helper server as a middleware to the karma web server
     middleware: ['helper-server'],
@@ -31,5 +33,23 @@ module.exports = function(config) {
         return helperServer;
       }]}
     ]
-  })
+  };
+
+  if (process.env.TRAVIS) {
+    configuration.reporters.push('saucelabs');
+    configuration.plugins.push('karma-sauce-launcher');
+    configuration.sauceLabs = {
+      tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER || Date.now()
+    };
+    configuration.customLaunchers = {
+      sl_chrome: { base: 'SauceLabs', browserName: 'chrome', platform: 'Windows 7', version: '35' },
+      sl_firefox: { base: 'SauceLabs', browserName: 'firefox', version: '30' },
+      sl_ios_safari: { base: 'SauceLabs', browserName: 'iphone', platform: 'OS X 10.9', version: '7.1' },
+      sl_ie_11: { base: 'SauceLabs', browserName: 'internet explorer', platform: 'Windows 8.1', version: '11' },
+      sl_android: { base: 'SauceLabs', browserName: 'Browser', platform: 'Android', version: '4.4', deviceName: 'Samsung Galaxy S3 Emulator', deviceOrientation: 'portrait' }
+    };
+    configuration.browsers = Object.keys(configuration.customLaunchers);
+  }
+
+  config.set(configuration);
 }
